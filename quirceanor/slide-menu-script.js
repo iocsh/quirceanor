658c706b58ce03b120ed9369c25149f7d919e639
iocsh <145376098+iocsh@users.noreply.github.com>
2024-01-27 19:20:16 +0800

    Add files via upload

---
+++ quirceanor/slide-menu-script.js
@@ -0,0 +1,7 @@
+function openSlideMenu() {
+    document.querySelector('.slide-menu').style.width = '250px';
+}
+
+function closeSlideMenu() {
+    document.querySelector('.slide-menu').style.width = '0';
+}
